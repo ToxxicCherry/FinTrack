e02c10370be2416805eff4f37ff17c934c7cacf8
Pavel Kaburneyeu <kps23160@gmail.com>
2025-03-03 19:09:04 +0100

    add category model

---
+++ app/db/models/category.py
@@ -0,0 +1,10 @@
+from app.db import Base
+from sqlalchemy import Integer, String, Column
+
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True, nullable=False)
